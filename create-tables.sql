142bf7bb0f445a05aad89489f54a2e6660f06423
O. Echevarria <spanishgentleman@yahoo.com>
2024-05-05 22:00:09 -0400

    Create create-tables.sql

---
+++ create-tables.sql
@@ -0,0 +1,15 @@
+create table scholarships (
+
+F_ID int(11) primary key,
+F_FirstName varchar(80),
+F_LastName varchar(80),
+F_PeoplesoftId varchar(80),
+F_email varchar(80),
+F_City varchar(80),
+F_State varchar(80),
+F_PostalCode varchar(80),
+F_Amount varchar(80),
+F_Semester varchar(80),
+F_Year varchar(80)
+
+);
